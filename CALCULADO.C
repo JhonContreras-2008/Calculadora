c6ab5b843a1d43638a4f5bc034d955bc29ab6ad3
JhonContreras <jhonstivenconva@gmail.com>
2026-04-30 11:21:16 -0500

    Ingresar parametros

---
+++ CALCULADO.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    // Definir parametros
+    int num1,num2;
+    
+    // Ingresar parametros
+    printf("Ingrese el primer parametro");
+    scanf("%lf", &num1);
+    printf("Ingrese el segundo parametro");
+    scanf("%lf", &num2);
+}
